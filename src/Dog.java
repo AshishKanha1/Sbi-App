a7429cd51e99ae574f4d2a30865abe133e6fd944
AshishKanha1 <ashishpattnayak9@gmail.com>
2022-01-25 21:23:57 +0530

    updated

--- src/Dog.java
+++ src/Dog.java
@@ -10,8 +10,15 @@ public class Student {
         System.out.println("do it better way");
 
     }
+
 public int add(){
 return 2;
 }
 
+public  void main2(){
+        System.out.println("i am main method in student class");
+        System.out.println("do it");
+        System.out.println("do it better way");
+
+    }
 }
